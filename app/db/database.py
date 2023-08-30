9a6b2c13eeae41176250fb09e9aec6f211133a8c
LozanoJohan <lozanojohan321@gmail.com>
2023-08-29 21:42:00 -0500

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, storage, firestore
+from pathlib import Path
 
+# Ruta absoluta de el archivo con los datos
+json_path = Path(__file__).resolve().parent / 'data' / 'firebase_config.json'
 
-cred = credentials.Certificate('firebase_config.json')
+cred = credentials.Certificate(json_path)
 storage_bucket = {
                     'storageBucket': 'colorextractor-df7b6.appspot.com'
                 }
